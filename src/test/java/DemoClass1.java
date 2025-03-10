5426851588076559d3bcec62cbf9ea2569432933
Viraj090909 <keneviraj@gmail.com>
2025-03-10 16:28:09 +0530

    mm

--- src/test/java/DemoClass1.java
+++ src/test/java/DemoClass1.java
@@ -1,10 +1,15 @@
 
 public class DemoClass1 {
 	
+	
+	
 	public static void main (String ...arg) {
-		System.out.println("welcome to demo class and now i editing this print statement so i can try pulling it ");
+		
+		System.out.println("welcome to demo class");
 		
 	}
+	
+	
 	
 
 }
