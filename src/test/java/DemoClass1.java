e6e95af5d84f6460e378f5c670300d18b73ff4d9
Viraj090909 <140181504+Viraj090909@users.noreply.github.com>
2025-03-10 16:39:16 +0530

    Update DemoClass1.java

--- src/test/java/DemoClass1.java
+++ src/test/java/DemoClass1.java
@@ -10,6 +10,8 @@ public class DemoClass1 {
 
 		System.out.println("welcome to demo class and now i editing this print statement so i can try pulling it ");
 
+		sysout.out.println("pulling again");
+
 		
 	}
 	
